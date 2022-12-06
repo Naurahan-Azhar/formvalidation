e6b22802bbd57488363ee161d857081d82154f52
Naurahan Azhar <naurahan.azhar@gmail.com>
2022-12-06 14:43:28 +0500

    Form Validation

--- form.js
+++
@@ -1,7 +0,0 @@
-function Form(){
-    const name = document.getElementById("name");
-    const dob = document.getElementById("dob");
-    const current date = document.getElementById("start date");
-    const end date = document.getElementById("End Date");
-    const 
-}
