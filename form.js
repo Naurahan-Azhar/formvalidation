d3943b70f7fbc3a1844876c8a72b6b97439c62cd
Naurahan Azhar <naurahan.azhar@gmail.com>
2022-12-06 14:21:31 +0500

    first project
    new

---
+++ form.js
@@ -0,0 +1,7 @@
+function Form(){
+    const name = document.getElementById("name");
+    const dob = document.getElementById("dob");
+    const current date = document.getElementById("start date");
+    const end date = document.getElementById("End Date");
+    const 
+}
